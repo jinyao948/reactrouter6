02794e338dc92575ae8a501f0b5fd22b674fc44e
jinyao948 <jinyao948@gmail.com>
2021-11-17 15:22:30 +0800

    links are still the same it seems

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {Route, Routes} from 'react-router-dom'
 import AllMeetup from './pages/AllMeetup';
 import Favourites from './pages/Favourites';
 import NewMeetUp from './pages/NewMeetUp';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div >
+      <MainNavigation/>
       <Routes>
       <Route path='/' element={<AllMeetup />}/>
         <Route path='/new-meetup' element={<NewMeetUp />}/>
---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+function MainNavigation() {
+    return (
+        <header>
+            <div>
+                React Meetups
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">All Meetups</Link>
+                    </li>
+                      <li>
+                        <Link to="/new-meetup">New Meetups</Link>
+                    </li>
+                      <li>
+                        <Link to="/favourites">Fav Meetups</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation
